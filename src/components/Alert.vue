0db5fe9dc8cd9884e514d09d74a9dcadd1086aa5
Elmer Jacobo <ejacobotiniano@gmail.com>
2023-10-20 10:01:08 -0500

    first commit

---
+++ src/components/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="alert">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.alert {
+  text-align: center;
+  margin-bottom: 2rem;
+  color: var(--white);
+  background-color: rgb(174, 0, 0);
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.4rem;
+  padding: .5rem
+}
+</style>
